f7a76823a3cf579b7b901db118b006b769742f2b
Randy Barents <33863457+koekenpan100@users.noreply.github.com>
2020-10-09 15:31:06 +0200

    Train class with add wagon method

--- Circustrein_S2_Software/Circustrein_S2_Software/Train.cs
+++ Circustrein_S2_Software/Circustrein_S2_Software/Train.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Circustrein_S2_Software
 {
-    class Train
+    public class Train
     {
+        public List<Wagon> Wagons { get; private set; }
+
+        public Train()
+        {
+            Wagons = new List<Wagon>();
+        }
+        public bool AddWagon(Wagon wagon)
+        {
+            if (wagon != null)
+            {
+                Wagons.Add(wagon);
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Train has the following wagons:{0}", Wagons.ToString());
+        }
     }
 }
